95eae884098124f7e269c8ebfbea1a772145eb12
saipu-rajeshkumar <151706428+saipu-rajeshkumar@users.noreply.github.com>
2024-08-22 11:57:02 +0530

    Create static block.java

---
+++ static block.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Staticclassdemo
+{
+    public static void main(String args[])
+    {
+        System.out.println("Inside the main method");
+        }
+        static{
+            System.out.println("Inside static block");
+        }
+    }
