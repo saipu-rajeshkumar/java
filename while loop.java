eebc5ae5ad56d5558d2972949f6d796a36bc08e7
saipu-rajeshkumar <151706428+saipu-rajeshkumar@users.noreply.github.com>
2024-08-22 11:26:19 +0530

    Create while loop.java

---
+++ while loop.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class whiledemo
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        while(i<3){
+            System.out.println("Helloworld");
+            i++;
+        }
+    }
+}
