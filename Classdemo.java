205998477cada4b455cdb7ed4e62d7339648c983
saipu-rajeshkumar <151706428+saipu-rajeshkumar@users.noreply.github.com>
2024-09-19 11:16:22 +0530

    Create Classdemo.java

---
+++ Classdemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ class Classdemo
+{
+	static String rollno;
+	static double avg;
+	public static void main(String args[])
+	{
+		rollno="23K61A05G5";
+		avg=94.6;
+		System.out.println("Roll no is "+rollno+"\nAverage is "+avg);
+	}
+}
