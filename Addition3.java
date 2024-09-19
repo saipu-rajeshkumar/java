b0a6a0c3588656ae1c21eb5c419c74037af9d89a
saipu-rajeshkumar <151706428+saipu-rajeshkumar@users.noreply.github.com>
2024-09-19 11:11:37 +0530

    Create Addition3.java

---
+++ Addition3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+public class Addition3
+{
+	public static void main(String args[])
+	{
+	    Scanner sc=new Scanner(System.in);
+		int a=5;
+		int b=10;
+		int c=a+b;
+		System.out.println("Sum of"+a+"and"+b+"is"+c);
+	}
+} 
